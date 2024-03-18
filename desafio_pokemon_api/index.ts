1ba35632829e9ad6f0800b535f69ef5a0def53ce
andremaurell <andremaurell3@gmail.com>
2024-03-18 11:44:16 -0300

    ✨ Feat: Add routes to take pokemon infos by name and every pokemons from any type, and route to take all pokemons 'limit 151', first generation

--- desafio_pokemon_api/index.ts
+++ desafio_pokemon_api/index.ts
@@ -3,11 +3,13 @@ import { Pool } from 'pg';
 import { router } from './src/routes';
 import { createTeamsTable } from './src/utils';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(router);
 const PG_URL = "postgres://default:17LolrYQGieb@ep-restless-hat-a43p2mw7.us-east-1.aws.neon.tech:5432/verceldb"
